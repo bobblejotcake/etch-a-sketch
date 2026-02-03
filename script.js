f553c4dc533a07035daa24349ee6d0b9818e95c0
bobblejotcake <seanlimhe@gmail.com>
2026-02-03 12:45:32 +1000

    Finished random colors

--- script.js
+++ script.js
@@ -14,9 +14,50 @@ function addGrids(number){
     for(let i = 0; i < number; i++){
         let gridAddition = document.createElement("div");
         gridAddition.classList.add("default");
+        
+
+
+        gridAddition.addEventListener("mouseenter", (e) =>{
+        e.preventDefault();
+        let randomRed = Math.floor(Math.random() * 355);
+        let randomBlue = Math.floor(Math.random() * 355);
+        let randomGreen = Math.floor(Math.random() * 355);
+        e.target.style.backgroundColor = `rgba(${randomRed},
+        ${randomGreen},${randomBlue},1)`;
+        })
         grid.append(gridAddition);
-        console.log("hi");
     }
 }
 
-addGrids(16);
+addGrids(100);
+
+
+function changeOpacity(currentOpacity){
+
+}
+
+
+const button  = document.querySelector("button");
+button.addEventListener("click", (e) => {
+    e.preventDefault;
+    console.log(e.target);
+    let ans = parseInt(prompt("How many boxes do you want?"));
+    if (ans == NaN || ans < 0 || ans > 100){
+        console.log("Please input a valid number from 0-100");
+        return;
+    }
+
+    //Delete all boxes
+    clearBoxes();
+
+    //spawn in new one
+    addGrids(ans);
+})
+
+
+function clearBoxes(){
+    const grid = document.querySelector(".container");
+    while(grid.firstChild){
+        grid.removeChild(grid.lastChild);
+    }
+}
